7c58c3c4c4b8aa810808ab3bd60ec7ad0013ed07
Tiago Berwanger <tiago.berwanger@mercos.com>
2021-09-10 19:46:16 -0300

    Adding bootstrap and style to the pages

--- fullstack/settings.py
+++ fullstack/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'fullstack.authentication'
+    'crispy_forms',
+    'fullstack.authentication',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
