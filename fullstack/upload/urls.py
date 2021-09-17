db3bd2a692ebb005c1b052bcee56ddce64ab2015
Tiago Berwanger <tiago.berwanger@mercos.com>
2021-09-17 17:30:16 -0300

    changing views to class-based views

--- fullstack/upload/urls.py
+++ fullstack/upload/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from fullstack.upload import views
 
 urlpatterns = [
-    path('add', views.add_contract, name='add_contract'),
-    path('list', views.list_contracts, name='contract'),
-    path('delete/<int:pk>', views.delete_contract, name='delete_contract'),
+    path('add', views.AddContractView.as_view(), name='add_contract'),
+    path('list', views.ListContractView.as_view(), name='contract'),
+    path('delete/<int:pk>', views.DeleteContractView.as_view(), name='delete_contract'),
 ]
--- fullstack/upload/views.py
+++ fullstack/upload/views.py
@@ -1,35 +1,46 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views import View
 
 from fullstack.core.forms import ContractForm
 from fullstack.core.models import Contract
 
 
-@login_required  # decorator que exige login para acessar determinada url
-def add_contract(request):
-    if request.method == 'POST':
-        form = ContractForm(request.POST, request.FILES)
+class AddContractView(View):
+    contract_form = ContractForm
+    contract_template = 'upload_form.html'
+
+    @method_decorator(login_required)
+    def get(self, request):
+        form = self.contract_form()
+        return render(request, self.contract_template, {'form': form})
+
+    @method_decorator(login_required)
+    def post(self, request):
+        form = self.contract_form(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('contract')
-    else:
-        form = ContractForm()
-    return render(request, 'upload_form.html', {
-        'form': form
-    })
+            return HttpResponseRedirect('list')
+        return render(request, self.contract_template, {'form': form})
 
 
-@login_required
-def list_contracts(request):
-    contracts = Contract.objects.all()
-    return render(request, 'contracts.html', {
-        'contracts': contracts
-    })
+class ListContractView(View):
+    contract_list = Contract
+    contract_template = 'contracts.html'
+
+    @method_decorator(login_required)
+    def get(self, request):
+        list_all_contracts = self.contract_list.objects.all()
+        return render(request, self.contract_template, {'contracts': list_all_contracts})
 
 
-@login_required()
-def delete_contract(request, pk):
-    if request.method == 'POST':
-        contract = Contract.objects.get(pk=pk)
+class DeleteContractView(View):
+    contract_list = Contract
+
+    @method_decorator(login_required)
+    def post(self, request, pk):
+        contract = self.contract_list.objects.get(pk=pk)
         contract.delete()
-    return redirect('contract')
+        return redirect('contract')
